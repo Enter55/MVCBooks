4faa70bfc542c2806f26b787b0082e95048abd38
Enter55 <SGA4@outlook.de>
2024-06-18 03:18:39 +0200

    Created BaseModel to standardize attributes across Book instances.

---
+++ BooksWorldMVC.com/BooksWorldMVC.com/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using BooksWorldMVC.com.Models.Base;
+
+namespace BooksWorldMVC.com.Models
+{
+    public class Book : BaseModel
+    {
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string Description { get; set; }
+    }
+}
